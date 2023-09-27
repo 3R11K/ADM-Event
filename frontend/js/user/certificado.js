1f873e88f6cdeffe06c52a773cbddf9590deda47
3R11K <erikbatista1222@gmail.com>
2023-09-27 18:09:38 -0300

    Update certificado.js

--- frontend/js/user/certificado.js
+++ frontend/js/user/certificado.js
@@ -1,4 +1,17 @@
-document.getElementById("home").onclick = function() {
+document.addEventListener("DOMContentLoaded", function() {
+  const xml = new XMLHttpRequest()
+  xml.open("GET", "/api/event", true)
+  xml.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      console.log("evento")
+    }else{
+      window.location.href = "/home"
+    }
+  }
+
+  document.getElementById("home").onclick = function() {
     // Redirecionando para a nova rota
     window.location.href = "/home";
   };
+})
+
