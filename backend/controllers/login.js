3420a2adbeb60323764dc689088af52fbc031194
3R11K <erikbatista1222@gmail.com>
2023-09-27 12:56:11 -0300

    Update login.js

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -1,36 +1,44 @@
-//login module
-const { getAuth, signInWithEmailAndPassword, signOut} = require("firebase/auth");
+// login module
+const { getAuth, signInWithEmailAndPassword, signOut } = require("firebase/auth");
 const auth = getAuth();
 
-//login function
-function login(req, res){ 
+// Configurar a persistência de sessão como "local"
+const setLocalPersistence = () => {
+  return auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+};
 
+// login function
+function login(req, res) {
   const email = req.body.email;
   const password = req.body.password;
 
-  signInWithEmailAndPassword(auth, email, password)
-
+  setLocalPersistence() // Configurar a persistência como "local"
+    .then(() => {
+      return signInWithEmailAndPassword(auth, email, password);
+    })
     .then((userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        res.status(200).send(user);
-        
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-        res.status(400).send(errorMessage);
+      // Signed in
+      const user = userCredential.user;
+      res.status(200).send(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      res.status(400).send(errorMessage);
     });
 }
 
-//logout function
-function logout(req, res){ 
-  signOut(auth).then(() => {
-    res.status(200).send("Logout realizado com sucesso");
-  }).catch((error) => {
-    console.log(error)
-  });
+// logout function
+function logout(req, res) {
+  signOut(auth)
+    .then(() => {
+      res.status(200).send("Logout realizado com sucesso");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
-module.exports = {login, logout}
+module.exports = { login, logout };
