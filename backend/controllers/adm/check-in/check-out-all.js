17a4aa58e306972c779545f11b50ab424f7d7f10
3R11K <erikbatista1222@gmail.com>
2023-10-02 15:51:57 -0300

    check out

--- backend/controllers/adm/check-in/check-out-all.js
+++ backend/controllers/adm/check-in/check-out-all.js
@@ -1,67 +1,58 @@
 const { getDatabase, ref, get, set } = require("firebase/database");
 const db = getDatabase();
 
-async function checkOutAll(req,res){
+async function checkOutAll(req, res) {
+  try {
     const date = new Date();
     const day = date.getDate();
-    const month = date.getMonth()+1;
+    const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const fDate = day + " " + month + " " + year;
 
-    //minutos até agora no dia
-    const minutes = date.getHours()*60 + date.getMinutes();
+    // Minutos até agora no dia
+    const minutes = date.getHours() * 60 + date.getMinutes();
 
     let usersRef = ref(db, 'checkIn/');
+    let data = {};
 
-    let data = {}
-    await get(usersRef).then((snapshot) => {
-        if (snapshot.exists()){
-            data = snapshot.val();
-        }else{
-            res.sendStatus(400);
-        }
-    });
-
-    try{
-
-        for(let user in data){
-            const checkin = data[user][fDate]
-            let diference = minutes - checkin;
-            await get(ref(db,"users/"+ user+"/acumuladas")).then((snapshot) => {
-                //se a snapshot existir
-                if(snapshot.exists()){
-                    let acumuladas = snapshot.val();
-                    diference += acumuladas;
-                    set(ref(db,"users/"+ user+"/acumuladas"), diference).then(()=>{
-                        console.log("acumuladas setadas");
-                        deleteCheckin(user);
-                    }).catch((err)=>{
-                        console.log(err, "erro ao setar acumuladas de "+ user);
-                    })
-                }else{
-                    set(ref(db,"users/"+ user+"/acumuladas"), diference).then(()=>{
-                        console.log("acumuladas setadas");
-                        deleteCheckin(user);
-
-                    }).catch((err)=>{
-                        console.log(err, "erro ao setar acumuladas de "+ user);
-                    })
-                }
-            });
-        };
-        res.status(200).send("Check-out de todos os usuários realizado com sucesso");
-    }catch(err){
-        console.log(err);
-        res.status(400).send("Erro ao fazer check-out de todos os usuários");
+    // Obtenha os dados de check-in
+    const snapshot = await get(usersRef);
+    if (snapshot.exists()) {
+      data = snapshot.val();
+    } else {
+      return res.status(400).send("Nenhum dado de check-in encontrado");
     }
+
+    for (let user in data) {
+      const checkin = data[user][fDate];
+      let difference = minutes - checkin;
+
+      // Obtenha as acumuladas do usuário
+      const acumuladasSnapshot = await get(ref(db, "users/" + user + "/acumuladas"));
+
+      if (acumuladasSnapshot.exists()) {
+        let acumuladas = acumuladasSnapshot.val();
+        difference += acumuladas;
+      }
+
+      // Atualize as acumuladas do usuário
+      await set(ref(db, "users/" + user + "/acumuladas"), difference);
+
+      // Exclua o check-in
+      await deleteCheckin(user);
+    }
+
+    res.status(200).send("Check-out de todos os usuários realizado com sucesso");
+  } catch (err) {
+    console.error(err);
+    res.status(400).send("Erro ao fazer check-out de todos os usuários");
+  }
 }
 
-async function deleteCheckin(user){
-    let refDb = ref(db, 'checkIn/'+user);
-    await set(refDb, null).then(()=>{
-        console.log("checkin deletado");
-    }).catch((err)=>{
-        console.log(err);
-    })
+async function deleteCheckin(user) {
+  let refDb = ref(db, 'checkIn/' + user);
+  await set(refDb, null);
+  console.log("Check-in deletado");
 }
+
 module.exports = checkOutAll;
